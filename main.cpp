5044a3e88550e19abdf68b3d8116b87d04dfda1d
Maxnxi <nisq@ya.ru>
2024-12-26 00:00:11 +0700

    add head render

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
-#include "tgaimage.h"
 #include <iostream>
 #include <filesystem>
 #include <unistd.h> 
+
+#include <vector>
+#include <cmath>
+#include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 //#include <gperftools/profiler.h>
 
 /*
@@ -12,7 +17,9 @@ https://github.com/ssloy/tinyrenderer/wiki
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-
+Model *model = NULL;
+const int width = 800;
+const int height = 800;
 /*
 https://github.com/ssloy/tinyrenderer/blob/d0703acf18c48f2f7d00e552697d4797e0669ade/main.cpp
 */
@@ -26,28 +33,89 @@ https://github.com/ssloy/tinyrenderer/blob/d0703acf18c48f2f7d00e552697d4797e0669
 // }
 
 // version #2
+// void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+// 	bool steep = false;
+// 	if (std::abs(x0-x1)<std::abs(y0-y1)) {
+// 		std::swap(x0, y0);
+// 		std::swap(x1, y1);
+// 		steep = true;
+// 	}
+// 	if (x0>x1) {
+// 		std::swap(x0,x1);
+// 		std::swap(y0,y1);
+// 	}
+// 	int dx = x1-x0;
+// 	int dy = y1-y0;
+// 	float derror = std::abs(dy/float(dx));
+// 	float error = 0;
+// 	int y = y0;
+// 	for (int x=x0; x<=x1; x++) {
+// 		if (steep) {
+// 			image.set(y, x, color);
+// 		} else {
+// 			image.set(x, y, color);
+// 		}
+// 		error += derror;
+
+// 		if (error>.5) {
+// 			y += (y1>y0?1:-1);
+// 			error -= 1.;
+// 		}
+// 	}
+// }
+// version #3
+// void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+//     bool steep = false;
+//     if (std::abs(x0-x1)<std::abs(y0-y1)) {
+//         std::swap(x0, y0);
+//         std::swap(x1, y1);
+//         steep = true;
+//     }
+//     if (x0>x1) {
+//         std::swap(x0, x1);
+//         std::swap(y0, y1);
+//     }
+//     int dx = x1-x0;
+//     int dy = y1-y0;
+//     int derror2 = std::abs(dy)*2;
+//     int error2 = 0;
+//     int y = y0;
+//     for (int x=x0; x<=x1; x++) {
+//         if (steep) {
+//             image.set(y, x, color);
+//         } else {
+//             image.set(x, y, color);
+//         }
+//         error2 += derror2;
+
+//         if (error2 > dx) {
+//             y += (y1>y0?1:-1);
+//             error2 -= dx*2;
+//         }
+//     }
+// }
+
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-bool steep = false;
-if (std::abs(x0-x1)<std::abs(y0-y1)) {
-	std::swap(x0, y0);
-	std::swap(x1, y1);
-	steep = true;
-}
-if (x0>x1) {
-	std::swap(x0,x1);
-	std::swap(y0,y1);
-}
-for (int x=x0; x<=x1;x++) {
-	float t = (x-x0)/(float)(x1-x0);
-	int y = y0*(1.-t) + y1*t;
-	if(steep) {
-		image.set(y,x,color);
-	} else {
-		image.set(x,y,color);
-	}
-}
+    bool steep = false;
+    if (std::abs(x0-x1)<std::abs(y0-y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0>x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
 
-
+    for (int x=x0; x<=x1; x++) {
+        float t = (x-x0)/(float)(x1-x0);
+        int y = y0*(1.-t) + y1*t;
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
+    }
 }
 
 // initial version
@@ -82,12 +150,32 @@ int main(int argc, char** argv) {
     
     std::cout << "Creating image..." << std::endl;
     
-    TGAImage image(100, 100, TGAImage::RGB);
+    // TGAImage image(100, 100, TGAImage::RGB);
     // image.set(52, 41, red);
 
-	line(10,10,80,10,image,red);
-	line(10,10,80,80, image, red);
-	line(10,80,80,80,image,red);
+	// line(10,10,80,10,image,red);
+	// line(10,10,80,80, image, red);
+	// line(10,80,80,80,image,red);
+
+	 if (2==argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("obj/african_head.obj");
+    }
+
+    TGAImage image(width, height, TGAImage::RGB);
+    for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j=0; j<3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j+1)%3]);
+            int x0 = (v0.x+1.)*width/2.;
+            int y0 = (v0.y+1.)*height/2.;
+            int x1 = (v1.x+1.)*width/2.;
+            int y1 = (v1.y+1.)*height/2.;
+            line(x0, y0, x1, y1, image, white);
+        }
+    }
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     
